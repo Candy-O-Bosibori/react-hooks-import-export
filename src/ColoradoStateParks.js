4d58bd6c4abb90097f9fe5082f1189cc34348ae6
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-01-30 22:01:27 +0300

    complete assigment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,13 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees, wildlife } from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks(); // => "42 parks!
+  wildlife()
+  console.log(trees)
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks;
